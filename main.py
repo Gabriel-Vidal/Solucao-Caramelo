aa18128585b85d697dfb6fe34b70400dd22430ab
LucasCerqueira1244 <carneirolucascerqueira@outlook.com>
2023-06-13 17:25:34 -0300

    Adicionando classe Pessoa

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ class Animal:
         self.cor = cor
         self.porte = porte
         self.particularidade = particularidade
+
+
+class Pessoa:
+    def __init__(self, nome, telefone, especie_interesse, preferencias):
+        self.nome = nome
+        self.telefone = telefone
+        self.especie_interesse = especie_interesse
+        self.preferencias = preferencias
