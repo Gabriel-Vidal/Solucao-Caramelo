7a9e39b5e62c66ae1c2aeafbab0ad7353a14fc6f
LucasCerqueira1244 <carneirolucascerqueira@outlook.com>
2023-06-13 17:32:30 -0300

    Criando funções para se cadastrar pessoas e animais

--- main.py
+++ main.py
@@ -19,3 +19,22 @@ class SolucaoCaramelo:
     def __init__(self):
         self.animais = []
         self.pessoas = []
+
+    def cadastrar_animal(self):
+        tipo = input("Digite o tipo do animal: ")
+        idade = input("Digite a idade aproximada do animal: ")
+        cor = input("Digite a cor do animal: ")
+        porte = input("Digite o porte do animal: ")
+        particularidade = input("Digite a particularidade do animal: ")
+
+        animal = Animal(tipo, idade, cor, porte, particularidade)
+        self.animais.append(animal)
+
+    def cadastrar_pessoa(self):
+        nome = input("Digite o nome da pessoa: ")
+        telefone = input("Digite o telefone da pessoa: ")
+        especie_interesse = input("Digite a espécie de interesse para adoção: ")
+        preferencias = input("Digite as preferências de animal: ")
+
+        pessoa = Pessoa(nome, telefone, especie_interesse, preferencias)
+        self.pessoas.append(pessoa)
