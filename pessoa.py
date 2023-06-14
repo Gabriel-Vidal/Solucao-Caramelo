0d627994e32a32ad6090d5b524b4a772c02058d5
LucasCerqueira1244 <carneirolucascerqueira@outlook.com>
2023-06-14 16:59:36 -0300

    adicionando arquivo pessoa.py

---
+++ pessoa.py
@@ -0,0 +1,6 @@
+class Pessoa:
+    def __init__(self, nome, telefone, especie_interesse, preferencias):
+        self.nome = nome
+        self.telefone = telefone
+        self.especie_interesse = especie_interesse
+        self.preferencias = preferencias
